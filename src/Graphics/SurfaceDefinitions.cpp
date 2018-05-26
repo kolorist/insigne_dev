f4b5606ea5371f554608045a24722e21d4be7b48
nhat.nguyenhong <hongnhatbkhn@gmail.com>
2018-05-26 14:09:38 +0700

    + insigne code refactor

--- src/Graphics/SurfaceDefinitions.cpp
+++ src/Graphics/SurfaceDefinitions.cpp
@@ -1,9 +1,13 @@
 #include "SurfaceDefinitions.h"
 
 namespace insigne {
-	template <>
-	gpu_command_buffer_t draw_command_buffer_t<stone::ImGuiSurface>::command_buffer[BUFFERED_FRAMES];
 
-	template <>
-	gpu_command_buffer_t draw_command_buffer_t<stone::SolidSurface>::command_buffer[BUFFERED_FRAMES];
+	namespace detail {
+		template <>
+			gpu_command_buffer_t draw_command_buffer_t<stone::ImGuiSurface>::command_buffer[BUFFERED_FRAMES];
+
+		template <>
+			gpu_command_buffer_t draw_command_buffer_t<stone::SolidSurface>::command_buffer[BUFFERED_FRAMES];
+	}
+
 }
