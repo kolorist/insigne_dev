209152159ec8420072a39b1ee13739cb5e949392
nhat.nguyenhong <hongnhatbkhn@gmail.com>
2018-10-31 14:17:01 +0700

    sh generation and visualization

--- src/Graphics/Tests/SHMath.cpp
+++ src/Graphics/Tests/SHMath.cpp
@@ -35,11 +35,14 @@ static const_cstr s_CubeMapFS = R"(
 layout (location = 0) out mediump vec4 o_Color;
 
 uniform mediump samplerCube u_Tex;
+uniform mediump samplerCube u_Tex2;
 in mediump vec3 v_Normal;
 
 void main()
 {
-	mediump vec3 outColor = texture(u_Tex, v_Normal).rgb;
+	mediump vec3 color1 = texture(u_Tex, v_Normal).rgb;
+	mediump vec3 color2 = texture(u_Tex2, v_Normal).rgb;
+	mediump vec3 outColor = color2;
 	o_Color = vec4(outColor, 1.0f);
 }
 )";
@@ -74,35 +77,31 @@ layout(std140) uniform ub_SHData
 
 in mediump vec3 v_Normal;
 
-const mediump float c1 = 0.429043f;
-const mediump float c2 = 0.511664f;
-const mediump float c3 = 0.743125f;
-const mediump float c4 = 0.886227f;
-const mediump float c5 = 0.247708f;
+const mediump float c0 = 0.2820947918f;
+const mediump float c1 = 0.4886025119f;
+const mediump float c2 = 2.185096861f;	// sqrt(15/pi)
+const mediump float c3 = 1.261566261f;	// sqrt(5/pi)
 
 mediump vec3 evalSH(in mediump vec3 i_normal)
 {
 	return
-		// constant term, lowest frequency //////
-		c4 * iu_Coeffs[0].xyz +
+		c0 * iu_Coeffs[0].xyz					// band 0
 
-		// axis aligned terms ///////////////////
-		2.0f * c2 * iu_Coeffs[1].xyz * i_normal.y +
-		2.0f * c2 * iu_Coeffs[2].xyz * i_normal.z +
-		2.0f * c2 * iu_Coeffs[3].xyz * i_normal.x +
+		- c1 * i_normal.y * iu_Coeffs[1].xyz * 0.667f
+		+ c1 * i_normal.z * iu_Coeffs[2].xyz * 0.667f
+		- c1 * i_normal.x * iu_Coeffs[3].xyz * 0.667f
 
-		// band 2 terms /////////////////////////
-		2.0f * c1 * iu_Coeffs[4].xyz * i_normal.x * i_normal.y +
-		2.0f * c1 * iu_Coeffs[5].xyz * i_normal.y * i_normal.z +
-		c3 * iu_Coeffs[6].xyz * i_normal.z * i_normal.z - c5 * iu_Coeffs[6].xyz +
-		2.0f * c1 * iu_Coeffs[7].xyz * i_normal.x * i_normal.z +
-		c1 * iu_Coeffs[8].xyz * (i_normal.x * i_normal.x - i_normal.y * i_normal.y);
+		+ c2 * i_normal.x * i_normal.y * 0.5f * iu_Coeffs[4].xyz * 0.25f
+		- c2 * i_normal.y * i_normal.z * 0.5f * iu_Coeffs[5].xyz * 0.25f
+		+ c3 * (-1.0f + 3.0f * i_normal.z * i_normal.z) * 0.25f * iu_Coeffs[6].xyz * 0.25f
+		- c2 * i_normal.x * i_normal.z * 0.5f * iu_Coeffs[7].xyz * 0.25f
+		+ c2 * (i_normal.x * i_normal.x - i_normal.y * i_normal.y) * 0.25f * iu_Coeffs[8].xyz * 0.25f;
 }
 
 void main()
 {
 	mediump vec3 c = evalSH(v_Normal);
-	o_Color = vec4(v_Normal, 1.0f);
+	o_Color = vec4(c, 1.0f);
 }
 )";
 
@@ -119,6 +118,8 @@ void SHMath::OnInitialize()
 {
 	m_Vertices.init(1024u, &g_StreammingAllocator);
 	m_Indices.init(4096u, &g_StreammingAllocator);
+
+	floral::vec3f cubeCoord = floral::texel_coord_to_cube_coord(2, 128, 128, 256);
 
 	{
 		{
@@ -188,15 +189,15 @@ void SHMath::OnInitialize()
 		insigne::ub_handle_t newUB = insigne::create_ub(desc);
 
 		SHData shData;
-		shData.CoEffs[0] = floral::vec4f(10.261054f, 14.317350f, 10.261044f, 0.0f);
-		shData.CoEffs[1] = floral::vec4f(-6.633853f, -5.090485f, -6.633842f, 0.0f);
-		shData.CoEffs[2] = floral::vec4f(13.300049f, 19.393381f, 13.300061f, 0.0f);
-		shData.CoEffs[3] = floral::vec4f(1.463530f, -0.079820f, 1.463536f, 0.0f);
-		shData.CoEffs[4] = floral::vec4f(0.234119f, -0.444766f, 0.234110f, 0.0f);
-		shData.CoEffs[5] = floral::vec4f(-10.240585f, -7.494582f, -10.240602f, 0.0f);
-		shData.CoEffs[6] = floral::vec4f(9.065324f, 14.902674f, 9.065321f, 0.0f);
-		shData.CoEffs[7] = floral::vec4f(2.581493f, -0.164537f, 2.581484f, 0.0f);
-		shData.CoEffs[8] = floral::vec4f(-2.101417f, -2.101410f, -2.101410f, 0.0f);
+		shData.CoEffs[0] = floral::vec4f(0.295409f, 0.147975f, 0.147705f, 0.0f);    
+		shData.CoEffs[1] = floral::vec4f(-0.425180f, -0.212434f, -0.212590f, 0.0f); 
+		shData.CoEffs[2] = floral::vec4f(-0.171174f, 0.085743f, 0.085587f, 0.0f);   
+		shData.CoEffs[3] = floral::vec4f(0.000000f, -0.085276f, 0.085587f, 0.0f);   
+		shData.CoEffs[4] = floral::vec4f(-0.000000f, 0.152628f, -0.152395f, 0.0f);  
+		shData.CoEffs[5] = floral::vec4f(0.304790f, -0.152395f, -0.152395f, 0.0f);  
+		shData.CoEffs[6] = floral::vec4f(-0.181933f, -0.091017f, -0.090966f, 0.0f); 
+		shData.CoEffs[7] = floral::vec4f(-0.000000f, -0.059217f, 0.059450f, 0.0f);  
+		shData.CoEffs[8] = floral::vec4f(-0.315117f, -0.157471f, -0.157558f, 0.0f); 
 
 		insigne::copy_update_ub(newUB, &shData, sizeof(SHData), 0);
 		m_SHUB = newUB;
@@ -229,7 +230,7 @@ void SHMath::OnInitialize()
 
 	// cubemap
 	{
-		floral::file_info texFile = floral::open_file("gfx/envi/textures/demo/cubeuvchecker.cbskb");
+		floral::file_info texFile = floral::open_file("gfx/envi/textures/demo/monvalley.cbskb");
 		floral::file_stream dataStream;
 
 		dataStream.buffer = (p8)m_MemoryArena->allocate(texFile.file_size);
@@ -248,7 +249,7 @@ void SHMath::OnInitialize()
 		dataStream.read<s32>(&colorSpace);
 		dataStream.read<s32>(&colorChannel);
 		dataStream.read<f32>(&encodeGamma);
-		dataStream.read<s32>(&mipsCount);	
+		dataStream.read<s32>(&mipsCount);
 
 		insigne::texture_desc_t demoTexDesc;
 		demoTexDesc.width = 512;
@@ -271,10 +272,56 @@ void SHMath::OnInitialize()
 		m_MemoryArena->free_all();
 	}
 
+	insigne::dispatch_render_pass();
+
+	{
+		floral::file_info texFile = floral::open_file("gfx/envi/textures/demo/cubeuvchecker.cbskb");
+		floral::file_stream dataStream;
+
+		dataStream.buffer = (p8)m_MemoryArena->allocate(texFile.file_size);
+		floral::read_all_file(texFile, dataStream);
+		floral::close_file(texFile);
+
+		c8 magicChars[4];
+		dataStream.read_bytes(magicChars, 4);
+
+		s32 colorRange = 0;
+		s32 colorSpace = 0;
+		s32 colorChannel = 0;
+		f32 encodeGamma = 0.0f;
+		s32 mipsCount = 0;
+		dataStream.read<s32>(&colorRange);
+		dataStream.read<s32>(&colorSpace);
+		dataStream.read<s32>(&colorChannel);
+		dataStream.read<f32>(&encodeGamma);
+		dataStream.read<s32>(&mipsCount);
+
+		insigne::texture_desc_t demoTexDesc;
+		demoTexDesc.width = 512;
+		demoTexDesc.height = 512;
+		demoTexDesc.format = insigne::texture_format_e::hdr_rgb;
+		demoTexDesc.min_filter = insigne::filtering_e::linear_mipmap_linear;
+		demoTexDesc.mag_filter = insigne::filtering_e::linear;
+		demoTexDesc.dimension = insigne::texture_dimension_e::tex_cube;
+		demoTexDesc.has_mipmap = true;
+		const size dataSize = insigne::prepare_texture_desc(demoTexDesc);
+		p8 pData = (p8)demoTexDesc.data;
+		// > This is where it get *really* interesting
+		// 	Totally opposite of normal 2D texture mapping, CubeMapping define the origin of the texture sampling coordinate
+		// 	from the lower left corner. OmegaLUL
+		// > Reason: historical reason (from Renderman)
+		dataStream.read_bytes((p8)demoTexDesc.data, dataSize);
+
+		m_Texture2 = insigne::create_texture(demoTexDesc);
+
+		m_MemoryArena->free_all();
+	}
+
 	{
 		insigne::shader_desc_t desc = insigne::create_shader_desc();
 		desc.reflection.uniform_blocks->push_back(insigne::shader_param_t("ub_Scene", insigne::param_data_type_e::param_ub));
 		desc.reflection.textures->push_back(insigne::shader_param_t("u_Tex", insigne::param_data_type_e::param_sampler_cube));
+		desc.reflection.textures->push_back(insigne::shader_param_t("u_Tex2", insigne::param_data_type_e::param_sampler_cube));
 
 		strcpy(desc.vs, s_CubeMapVS);
 		strcpy(desc.fs, s_CubeMapFS);
@@ -290,8 +337,12 @@ void SHMath::OnInitialize()
 			s32 texSlot = insigne::get_material_texture_slot(m_CubeMaterial, "u_Tex");
 			m_CubeMaterial.textures[texSlot].value = m_Texture;
 		}
+		{
+			s32 texSlot = insigne::get_material_texture_slot(m_CubeMaterial, "u_Tex2");
+			m_CubeMaterial.textures[texSlot].value = m_Texture2;
+		}
 	}
-	
+
 	m_DebugDrawer.Initialize();
 }
 
@@ -304,10 +355,22 @@ void SHMath::OnUpdate(const f32 i_deltaMs)
 void SHMath::OnRender(const f32 i_deltaMs)
 {
 	insigne::begin_render_pass(DEFAULT_FRAMEBUFFER_HANDLE);
-	m_SceneData.XForm = m_CameraMotion.GetRotation().normalize().to_transform();
-	insigne::copy_update_ub(m_UB, &m_SceneData, sizeof(SceneData), 0);
-	insigne::draw_surface<DemoSurface>(m_VB, m_IB, m_CubeMaterial);
-	insigne::dispatch_render_pass();
+	{
+		SceneData sceneData;
+		sceneData.XForm = floral::construct_translation3d(0.0f, 0.0f, -1.5f) * m_CameraMotion.GetRotation().normalize().to_transform();
+		sceneData.WVP = m_SceneData.WVP;
+		insigne::copy_update_ub(m_UB, &sceneData, sizeof(SceneData), 0);
+		insigne::draw_surface<DemoSurface>(m_VB, m_IB, m_CubeMaterial);
+		insigne::dispatch_render_pass();
+	}
+	{
+		SceneData sceneData;
+		sceneData.XForm = floral::construct_translation3d(0.0f, 0.0f, 1.5f) * m_CameraMotion.GetRotation().normalize().to_transform();
+		sceneData.WVP = m_SceneData.WVP;
+		insigne::copy_update_ub(m_UB, &sceneData, sizeof(SceneData), 0);
+		insigne::draw_surface<DemoSurface>(m_VB, m_IB, m_Material);
+		insigne::dispatch_render_pass();
+	}
 	m_DebugDrawer.Render(m_DebugWVP);
 	insigne::end_render_pass(DEFAULT_FRAMEBUFFER_HANDLE);
 	insigne::mark_present_render();
--- src/Graphics/Tests/SHMath.h
+++ src/Graphics/Tests/SHMath.h
@@ -12,6 +12,30 @@
 
 namespace stone {
 
+#if 0
+const mediump float c1 = 0.429043f;
+const mediump float c2 = 0.511664f;
+const mediump float c3 = 0.743125f;
+const mediump float c4 = 0.886227f;
+const mediump float c5 = 0.247708f;
+
+	return
+		// constant term, lowest frequency //////
+		c4 * iu_Coeffs[0].xyz +
+
+		// axis aligned terms ///////////////////
+		2.0f * c2 * iu_Coeffs[1].xyz * i_normal.y +
+		2.0f * c2 * iu_Coeffs[2].xyz * i_normal.z +
+		2.0f * c2 * iu_Coeffs[3].xyz * i_normal.x +
+
+		// band 2 terms /////////////////////////
+		2.0f * c1 * iu_Coeffs[4].xyz * i_normal.x * i_normal.y +
+		2.0f * c1 * iu_Coeffs[5].xyz * i_normal.y * i_normal.z +
+		2.0f * c1 * iu_Coeffs[7].xyz * i_normal.x * i_normal.z +
+		c3 * iu_Coeffs[6].xyz * i_normal.z * i_normal.z - c5 * iu_Coeffs[6].xyz +
+		c1 * iu_Coeffs[8].xyz * (i_normal.x * i_normal.x - i_normal.y * i_normal.y);
+#endif
+
 class SHMath : public ITestSuite {
 	public:
 		SHMath();
@@ -51,6 +75,7 @@ class SHMath : public ITestSuite {
 		insigne::material_desc_t				m_Material;
 
 		insigne::texture_handle_t				m_Texture;
+		insigne::texture_handle_t				m_Texture2;
 		insigne::shader_handle_t				m_CubeShader;
 		insigne::material_desc_t				m_CubeMaterial;
 
